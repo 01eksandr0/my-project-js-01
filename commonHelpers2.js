49c0fa82372210787fae58aaa6ca56a5a5fa0dd3
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-02-01 13:38:50 +0000

    Deploying to gh-pages from @ 01eksandr0/goit-js-hw-11@fdc0d3709c3665276b3eebdea4e2cf9ceb9d95c0 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -12,24 +12,24 @@ import{d as x,b as $,h as C,s as W,a as v,c as U,o as N,n as H}from"./assets/cha
 				<!-- <div class="workout-card__rating-block"> -->
 				<p class="workout-card__rating-text"> ${i.toFixed(1)} </p>
 				<svg class="workout-card__rating-icon">
-					<use class="icon-star" href="././img/symbol-defs.svg#icon-star-gold"></use>
+					<use class="icon-star" href="../../img/symbol-defs.svg#icon-star-gold"></use>
 				</svg>
 				<svg class="workout-card__basket-icon" style="display: none">
-					<use class="icon-basket" href="./img/symbol-defs.svg#icon-trash"></use>
+					<use class="icon-basket" href="../../img/symbol-defs.svg#icon-trash"></use>
 				</svg>
 				<!-- </div> -->
 			</div>
 			<button type="button" class="workout-card__link-start" data-id="${a}">
 				Start
 				<svg class="workout-card__link-icon" width="16" height="16">
-					<use class="icon-arrow" href="././img/symbol-defs.svg#icon-arrow"></use>
+					<use class="icon-arrow" href="../../img/symbol-defs.svg#icon-arrow"></use>
 				</svg>
 			</button>
 		</div>
 
 		<h3 class="workout-card__title">
 			<svg class="workout-card__title-icon">
-				<use class="icon-icon" href="././img/symbol-defs.svg#icon-icon"></use>
+				<use class="icon-icon" href="../../img/symbol-defs.svg#icon-icon"></use>
 			</svg>
 			${r[0].toUpperCase()+r.slice(1)}
 		</h3>
