45634bac3ce4c331bbeb6f8d2153e8b53abf4852
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-02-01 16:36:05 +0200

    1

--- src/js/getMarkup/createStarsMarkup.js
+++ src/js/getMarkup/createStarsMarkup.js
@@ -1,13 +1,12 @@
 export const createStarsMarkup = int => {
   const starsMarkup = [
     '/img/symbol-defs.svg#icon-star',
-    './../img/symbol-defs.svg#icon-star',
-    './../ img / symbol - defs.svg#icon - star',
-    './../img/symbol-defs.svg#icon-star',
-    './../img/symbol-defs.svg#icon-star',
+    '/img/symbol-defs.svg#icon-star',
+    '/img/symbol-defs.svg#icon-star',
+    '/img/symbol-defs.svg#icon-star',
+    '/img/symbol-defs.svg#icon-star',
   ];
-  const goldStar =
-    'https://01eksandr0.github.io/goit-js-hw-11/src/img/symbol-defs.svg#icon-star-gold';
+  const goldStar = '/img/symbol-defs.svg#icon-star-gold';
   const number = parseFloat(int);
   starsMarkup.forEach((_, index) => {
     if (index + 1 <= number) starsMarkup[index] = goldStar;
