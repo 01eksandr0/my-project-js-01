3a8d0c446b70a42321e2f35374582636fa3a96d1
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-02-05 16:53:29 +0200

    1

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,6 +1,6 @@
 const changeThemeBtn = document.querySelector('.change');
 const link = document.querySelector('#dynamicStyles');
 changeThemeBtn.addEventListener('click', () => {
-  link.href = './css/layout/dark-theme.css';
+  console.log(link.href);
 });
 // https://github.com/01eksandr0/my-project-js-01/blob/main/src/css/layout/dark-theme.css
