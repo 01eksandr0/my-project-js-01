78561d3569ffa5f4ea7370175d54be5156bb3c66
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-02-05 16:33:17 +0200

    й

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,9 +1,5 @@
 const changeThemeBtn = document.querySelector('.change');
 const link = document.querySelector('#dynamicStyles');
 changeThemeBtn.addEventListener('click', () => {
-  if (link.href === 'http://localhost:5173/css/layout/favorite-section.css') {
-    link.href = './css/layout/dark-theme.css';
-  } else {
-    link.href = './css/layout/favorite-section.css';
-  }
+  link.href = './css/layout/dark-theme.css';
 });
